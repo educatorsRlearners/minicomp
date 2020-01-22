0a52b368939bca12a62ee1b8eef356ad91c7f9ff
educatorsRlearners <p.evansimpson@gmail.com>
2020-01-22 16:23:23 +0100

    Convert store numbers to int

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,3 +4,10 @@ df = pd.read_csv()
 
 #Assortment - describes an assortment level: a = basic, b = extra, c = extended
 df['Assortment'] = df['Assortment'].replace({"a": 1, "b": 2, "c": 3})
+
+
+#Drop the rows where we do not have a store number
+df_train = df_train.dropna(subset=['Store'])
+
+#Convert the store numbers from float to int
+df_train.loc[:, 'Store'] = df_train.loc[:, 'Store'].astype(int)
