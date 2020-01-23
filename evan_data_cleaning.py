4b750e5d43fee26af91ead9b747f625382867e72
educatorsRlearners <p.evansimpson@gmail.com>
2020-01-23 10:08:23 +0100

    add Evan's file

--- evan_data_cleaning.py
+++ evan_data_cleaning.py
@@ -13,6 +13,8 @@ df_train = df_train.dropna(subset=['Store'])
 #Convert the store numbers from float to int
 df_train.loc[:, 'Store'] = df_train.loc[:, 'Store'].astype(int)
 
+<<<<<<< HEAD
+=======
 #define a function
 def clean_store_compyear(store):
 #store Dataframe fill Competition since Year
@@ -30,9 +32,16 @@ def clean_store_commonth(store_n):
     store_n = store_n.drop(['Promo2Week_Year','Promo2Week_Year_2'],axis=1)
     return store_n
 
+>>>>>>> 04b686db6a92d6a490838c489af1257cad58eea6
 
 #Convert the date colume to datetime
 df_train.loc[:, 'Date'] = pd.to_datetime(df_train.loc[:, 'Date'])
 
 #Convert the DayOfWeek column to Monday(0) - Sunday(6) and replace missing values 
 df_train.loc[:, 'DayOfWeek'] = df_train.loc[:, 'Date'].dt.dayofweek
+<<<<<<< HEAD
+
+#Create a numpy ns64 column
+df_train["timestamp"] = df_train.Date.values.astype(np.int64)
+=======
+>>>>>>> 04b686db6a92d6a490838c489af1257cad58eea6
