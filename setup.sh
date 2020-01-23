e2100d63643eb3143c18a111548105c9f7ad89b1
JohannaViktor <johanna.viktor@siemens.com>
2020-01-23 11:55:16 +0100

    mkdir -p

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 sudo apt-get update
-
+cd $HOME
 git clone https://github.com/ADGEfficiency/minicomp-rossman
 #kaggle competitions download -c data-science-bowl-2019
 cd minicomp-rossman
 python data.py
 cd data
-mkdir $HOME/minicomp
+mkdir -p $HOME/minicomp
 mv *.zip $HOME/minicomp
 sudo apt-get install unzip
 unzip $HOME/minicomp/*.zip -d $HOME/minicomp/raw-data
